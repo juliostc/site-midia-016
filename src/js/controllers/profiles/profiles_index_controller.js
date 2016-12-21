8b5f154dfbf72418d7819077229eb2ac4f4946c0
iMac <apple@iMac-de-APPLE.local>
2016-12-21 15:20:27 -0200

    atualizações de layout

--- src/js/controllers/profiles/profiles_index_controller.js
+++ src/js/controllers/profiles/profiles_index_controller.js
@@ -10,6 +10,14 @@
         ) {
             PageSettings.setSubtitle("Perfis");
             this.profiles = profileService.findAll();
+        
+            this.tracks = [
+                {title: "Projeto de Pesquisa", url: "PROJETO_DE_PESQUISA", index: 0},
+                {title: "Artigo Científico", url: "ARTIGO_CIENTIFICO", index: 1},
+                {title: "Projeto de Desenvolvimento", url: "ARTIGO_CIENTIFICO", index: 2},
+                {title: "Produto Midiático", url: "PRODUTO_MIDIATICO", index: 3, exception: true},
+                {title: "Relatório do Produto", url: "RELATORIO", index: 4}
+            ];
 
             this.currentID = Object.keys($routeParams)[0];
             $scope.selectedProfile = profileService.findById(this.currentID);
