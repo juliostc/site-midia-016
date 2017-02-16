1e7e37ebaabd3ffee0e5c7ce69a98956538c6bd4
iMac <apple@iMac-de-APPLE.local>
2017-02-15 23:41:19 -0200

    tela principal finalizada

--- src/js/controllers/profiles/profiles_index_controller.js
+++ src/js/controllers/profiles/profiles_index_controller.js
@@ -1,79 +1,84 @@
 (function (angular) {
-    "use strict";
-
-    angular.module("controllers.profiles.index", [
+	"use strict";
+	angular.module("controllers.profiles.index", [
     "services.profile"
-  ])
-        .controller("ProfilesIndexController", function (
-            PageSettings, profileService, $routeParams, $location, $scope, $sce, $window
-        ) {
-            PageSettings.setSubtitle("Perfis");
-            this.profiles = profileService.findAll();
-
-            $scope.getProfileUrl = function (id) {
-
-                return profileService.getProfileUrl(id);
-            };
-
-
-            this.tracks = [
-                {
-                    title: "Projeto de Pesquisa",
-                    url: "PROJETO_DE_PESQUISA",
-                    index: 0
-                },
-                {
-                    title: "Artigo Científico",
-                    url: "ARTIGO",
-                    index: 1
-                },
-                {
-                    title: "Projeto de Desenvolvimento",
-                    url: "PROJETO_DE_DESENVOLVIMENTO",
-                    index: 2
-                },
-                {
-                    title: "Produto Midiático",
-                    url: "PRODUTO_MIDIATICO",
-                    index: 3,
-                    exception: true
-                },
-                {
-                    title: "Relatório do Produto",
-                    url: "RELATORIO",
-                    index: 4
+  ]).controller("ProfilesIndexController", function (PageSettings, profileService, $routeParams, $location, $scope, $sce, $window) {
+		PageSettings.setSubtitle("Perfis");
+		this.profiles = profileService.findAll();
+		$scope.getProfileUrl = function (id) {
+			return profileService.getProfileUrl(id);
+		};
+		$scope.paddingFix = function (index) {
+			if ((index + 1) % 6 == 0) {
+				return "right";
+			}
+			if ((index) % 6 == 0) {
+				return "left";
+			}
+			return "";
+		};
+		this.tracks = [
+			{
+				title: "Projeto de Pesquisa"
+				, url: "PROJETO_DE_PESQUISA"
+				, index: 0
+                }
+			, {
+				title: "Artigo Científico"
+				, url: "ARTIGO"
+				, index: 1
+                }
+			, {
+				title: "Projeto de Desenvolvimento"
+				, url: "PROJETO_DE_DESENVOLVIMENTO"
+				, index: 2
+                }
+			, {
+				title: "Produto Midiático"
+				, url: "PRODUTO_MIDIATICO"
+				, index: 3
+				, exception: true
+                }
+			, {
+				title: "Relatório do Produto"
+				, url: "RELATORIO"
+				, index: 4
                 }
             ];
-
-            this.currentID = Object.keys($routeParams)[0];
-            $scope.selectedProfile = profileService.findById(this.currentID);
-            this.isActive = function () {
-                return !!($scope.selectedProfile);
-            };
-            if ($scope.selectedProfile) {
-                $scope.iframeUrl = $sce.trustAsResourceUrl($scope.selectedProfile.link_produto);
-                console.log($scope.iframeUrl + " - " + $scope.selectedProfile.link_produto);
-                console.log($scope.selectedProfile);
-            }
-            $scope.previousAndNext = profileService.previousAndNext($scope.selectedProfile);
-            console.log($scope.previousAndNext);
-            $scope.showMedia = function () {
-                var perfil = $scope.selectedProfile;
-                if (perfil.tipo_produto == 'EMBED') {
-                    perfil.tab = 'media';
-                } else if (perfil.tipo_produto == 'LINK') {
-                    $window.open(perfil.link_produto, '_blank');
-                }
-
-            };
-            $scope.hideMedia = function () {
-                $scope.selectedProfile.tab = 'main';
-
-            };
-
-
-
-
-
-        });
+		this.currentID = Object.keys($routeParams)[0];
+		$scope.selectedProfile = profileService.findById(this.currentID);
+		this.isActive = function () {
+			return !!($scope.selectedProfile);
+		};
+		if ($scope.selectedProfile) {
+			$scope.iframeUrl = $sce.trustAsResourceUrl($scope.selectedProfile.link_produto);
+		}
+		$scope.previousAndNext = profileService.previousAndNext($scope.selectedProfile);
+		$scope.showMedia = function () {
+			var perfil = $scope.selectedProfile;
+			if (perfil.tipo_produto == 'EMBED') {
+				perfil.tab = 'media';
+			}
+			else if (perfil.tipo_produto == 'LINK') {
+				$window.open(perfil.link_produto, '_blank');
+			}
+		};
+		$scope.hideMedia = function () {
+			$scope.selectedProfile.tab = 'main';
+		};
+	}).component('puff', {
+		templateUrl: 'components/puff.html'
+		, bindings: {
+			covers: '<'
+		}
+		, controller: function (profileService, $scope) {
+			var exceptions = [];
+			$scope.covers = [];
+			for (var i = 0; i < 3; i++) {
+				var profile = profileService.getRandomProfile(exceptions);
+				$scope.covers[i] = "assets/profiles/" + profile.id + "/CAPA.png";
+				exceptions.push(profile);
+			}
+		}
+	});
 })(window.angular);
--- src/js/services/profile_service.js
+++ src/js/services/profile_service.js
@@ -1,59 +1,56 @@
 (function (angular) {
-    "use strict";
-
-    angular.module("services.profile", [])
-        .service("profileService", function ($http) {
-
-            if (!profiles) profiles = [{
-                "teste": "teste"
+	"use strict";
+	angular.module("services.profile", []).service("profileService", function ($http) {
+		if (!profiles) profiles = [{
+			"teste": "teste"
             }];
-
-            this.findAll = function () {
-                return profiles;
-            };
-
-            this.findById = function (id) {
-                return profiles.find(function (profile) {
-                    return profile.id === id;
-                });
-            };
-
-            this.previousAndNext = function (profile) {
-                var index = profiles.indexOf(profile);
-                var result = [];
-
-                if (index > 0) {
-                    result[0] = profiles[index - 1];
-                }
-                if (index < profiles.length) {
-                    result[1] = profiles[index + 1];
-                }
-                return result;
-            };
-
-            this.getProfileUrl = function (id) {
-                return "#!/perfis/?{{" + id + "}}";
-            };
-        });
+		this.findAll = function () {
+			return profiles;
+		};
+		this.findById = function (id) {
+			return profiles.find(function (profile) {
+				return profile.id === id;
+			});
+		};
+		this.previousAndNext = function (profile) {
+			var index = profiles.indexOf(profile);
+			var result = [];
+			if (index > 0) {
+				result[0] = profiles[index - 1];
+			}
+			if (index < profiles.length) {
+				result[1] = profiles[index + 1];
+			}
+			return result;
+		};
+		this.getProfileUrl = function (id) {
+			return "#!/perfis/?{{" + id + "}}";
+		};
+		this.getRandomProfile = function (exceptions) {
+			var aux = profiles.slice(0);
+			for (var i = 0; i < exceptions.length; i++) {
+				aux.splice(aux.indexOf(exceptions[i]), 1);
+			}
+			var index = Math.max(Math.min(Math.ceil(Math.random() * aux.length), aux.length - 1), 0);
+			return aux[index];
+		};
+	});
 })(window.angular);
-
 var profiles = [];
 
 function listEntries(obj) {
-    //no HTML existe uma tag script que faz um get na planilha e chama uma função de callback (que no caso é essa)
-    profiles = [];
-    obj.feed.entry.forEach(function (profile) {
-        profiles.push({
-            id: profile["gsx$id"].$t,
-            nome: profile["gsx$nome"].$t,
-            bio: profile["gsx$bio"].$t,
-            nome_album: profile["gsx$nomealbum"].$t,
-            rede_social: profile["gsx$redesocial"].$t,
-            link_produto: profile["gsx$linkproduto"].$t,
-            tipo_produto: profile["gsx$tipoproduto"].$t
-
-        });
-    });
-
-    //variavel global :/ porco mas n soube fazer de outro jeito
+	//no HTML existe uma tag script que faz um get na planilha e chama uma função de callback (que no caso é essa)
+	profiles = [];
+	obj.feed.entry.forEach(function (profile) {
+		profiles.push({
+			id: profile["gsx$id"].$t
+			, nome: profile["gsx$nome"].$t
+			, bio: profile["gsx$bio"].$t
+			, nome_album: profile["gsx$nomealbum"].$t
+			, rede_social: profile["gsx$redesocial"].$t
+			, link_produto: profile["gsx$linkproduto"].$t
+			, tipo_produto: profile["gsx$tipoproduto"].$t
+		});
+	});
+	//variavel global :/ porco mas n soube fazer de outro jeito
 }
